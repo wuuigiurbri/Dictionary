20ac9bd146d0a5ed3d869de2988218d940656ca2
wuuigiurbri <120124489+wuuigiurbri@users.noreply.github.com>
2026-06-06 13:58:33 -0400

    Add files via upload

--- activites.py
+++ activites.py
@@ -13,15 +13,44 @@
 # print (vowels)
 
 #activity 2
-#count the occurences of each letter in the alphabet entered by the user
-string2= input("Enter a string ")
-letters= {}
+#count the occurences of each letter in the string entered by the user
+# string2= input("Enter a string ")
+# letters= {}
 
 
-for i in string2:
-    if i.isalpha():
-        if i in letters:
-            letters[i] +=1
-        else:
-            letters = 1
-print (letters)
+# for i in string2.lower():
+#     if i.isalpha():
+#         if i in letters:
+#             letters[i] +=1
+#         else:
+#             letters [i]= 1
+# print (letters)
+
+#activity 3
+#find if the number entered by the user in a pangram
+int1= input("enter a number ")
+numbers= {'0':0,
+          '1':0,
+          '2':0,
+          '3':0,
+          '4':0,
+          '5':0,
+          '6':0,
+          '7':0,
+          '8':0,
+          '9':0}
+
+for i in int1:
+    if i in numbers:
+        numbers[i] +=1
+
+pangram= True
+
+for i in numbers.values():
+    if i == 0:
+        pangram= False
+
+if pangram:
+    print("It's a pangram")
+else:
+    print("Number is not a pangram")
